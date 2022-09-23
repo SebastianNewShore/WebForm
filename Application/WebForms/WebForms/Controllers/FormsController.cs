3439f461797f9eb79ea6abaa2ed3ab97d4c0a1e2
SebastianNewShore <31068657+SebastianNewShore@users.noreply.github.com>
2022-09-23 08:49:43 -0500

    Se agrega en el modelo los campos faltantes

--- Application/WebForms/WebForms/Controllers/FormsController.cs
+++ Application/WebForms/WebForms/Controllers/FormsController.cs
@@ -25,17 +25,18 @@ namespace WebForms.Controllers
         //Http Post Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Employees employees)
+        public IActionResult Index(Employees employees)
         {
             try
             {
                 employees.Cellphone = RestrictionConstants.codeCountryCol + employees.Cellphone;
                 if (ModelState.IsValid)
                 {
+                    employees.HireDate = DateTime.Now;
                     TempData["Message"] = "El empleado se ha creado correctamente";
 
 
-                        if (SaveDatabase(employees))
+                        if (SaveDatabase(employees,new Salaries()))
                         {
                             TempData["Message"] = "El empleado se ha creado correctamente";
                         }
@@ -53,18 +54,18 @@ namespace WebForms.Controllers
             return View();
         }
 
-        private bool SaveDatabase(Employees employees)
+        private bool SaveDatabase(Employees employees,Salaries salary)
         {
             try
             {
                 _db.Employees.Add(employees);
-
-
+                salary.EmpNoNavigation = employees;
+                _db.Salaries.Add(salary);
                     //employees.IdCollaboratorNavigation = collaborator;
                     //_db.TbCollaboratorPositions.Add(position);
                 
 
-                _db.SaveChanges();
+                //_db.SaveChanges();
 
                 return true;
             }
