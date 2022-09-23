14bb7214d090ded8a16e275d471c9c2b0dad842b
SebastianNewShore <31068657+SebastianNewShore@users.noreply.github.com>
2022-09-22 21:45:52 -0500

    Se agrega conexion a base de datos

--- Application/WebForm/WebForm/Program.cs
+++ Application/WebForm/WebForm/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebForm.Models;
 
 namespace WebForm
 {
@@ -13,6 +14,13 @@ namespace WebForm
     {
         public static void Main(string[] args)
         {
+            using(var context = new dbWebFormContext())
+            {
+                foreach (var emp in context.Employees.ToList())
+                {
+                    Console.WriteLine(emp.FirstName);
+                }
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
